cb61e89ef048eb1eaa63757b2feba62c0e73ed0f
Rich Churcher <rich.churcher@gmail.com>
2017-01-18 15:57:20 +1300

    Action creators

--- client/containers/TaskContainer.js
+++ client/containers/TaskContainer.js
@@ -1,13 +1,23 @@
 import {connect} from 'react-redux'
 
 import Task from '../components/Task'
+import { submitTask, taskCorrect, taskIncorrect } from '../actions/task.js'
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     task: state.task
   }
 }
 
+const mapDispatchToProps = dispatch => {
+  return {
+    submitTask: () => dispatch(submitTask()),
+    taskCorrect: () => dispatch(taskCorrect()),
+    taskIncorrect: () => dispatch(taskIncorrect())
+  }
+}
+
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Task)
--- client/reducers/task.js
+++ client/reducers/task.js
@@ -1,7 +1,15 @@
+import * as types from '../actions/types'
+
 const tasks = (state = { id: 0, description: 'Task' }, action) => {
   switch (action.type) {
-    case 'TASK_CORRECT':
+    case types.TASK_CORRECT:
       return Object.assign({}, state, { correct: true })
+
+    case types.TASK_INCORRECT:
+      return Object.assign({}, state, { correct: false })
+
+    case types.TASK_SUBMITTED:
+      return Object.assign({}, state, { submitted: true })
 
     default:
       return state
