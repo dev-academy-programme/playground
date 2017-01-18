0dc2eb43fc93d4ba9744e7093d2add6afc440513
Rich Churcher <rich.churcher@gmail.com>
2017-01-19 10:57:58 +1300

    redux-thunk

--- client/index.js
+++ client/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
+import { applyMiddleware, compose, createStore } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 
 import reducers from './reducers'
 import App from './components/App'
 
-let store = createStore(reducers, window.devToolsExtension && window.devToolsExtension())
+let store = createStore(reducers, compose(
+  applyMiddleware(thunkMiddleware),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+))
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
